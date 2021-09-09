7f6b1891c01564d09542948d709048b0dd177cfe
luyomo <syunka.tyo@gmail.com>
2021-09-09 14:45:11 +0900

    Added the TiDB data fetching

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/luyomo/ticql
 go 1.16
 
 require (
+	github.com/didi/gendry v1.7.0 // indirect
 	github.com/felipejfc/go-tcp-example v0.0.0-20170513154354-996e7f18b0e1
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0
 	github.com/pierrec/lz4/v4 v4.1.8 // indirect
 	github.com/spf13/cobra v0.0.0-20170427180918-7b1b6e8dc027
---
+++ pkg/tidb/db.go
@@ -0,0 +1,120 @@
+package main 
+
+import (
+    "fmt"
+//    "time"
+    "log"
+     "database/sql"
+//    "reflect"
+      _ "github.com/go-sql-driver/mysql"
+     "github.com/didi/gendry/scanner"
+)
+
+const (
+    CUSTOM_TYPE = 0x0000
+    AsciiType = 0x0001
+    LongType = 0x0002
+    BytesType = 0x0003
+    BooleanType = 0x0004
+    CounterColumnType = 0x0005
+    DecimalType = 0x0006
+    DoubleType = 0x0007
+    FloatType = 0x0008
+    Int32Type = 0x0009
+    UTF8Type = 0x000A
+    DateType = 0x000B
+    UUIDType = 0x000C
+    VarcharType = 0x000D
+    IntegerType = 0x000E
+    TimeUUIDType = 0x000F
+    InetAddressType = 0x0010
+    SimpleDateType = 0x0011
+    TimeType = 0x0012
+    ShortType = 0x0013
+    ByteType = 0x0014
+    DurationType = 0x0015
+    ListType = 0x0020
+    MapType = 0x0021
+    SetType = 0x0022
+    UserType = 0x0030
+    TupleType = 0x0031
+)
+
+const (
+    HEADER_CODE = 0x00000002
+    OPTION = 0x00000001
+)
+
+
+type ColumnType struct {
+    ColumnName      string
+    TiDataType      string
+    Length          int
+    TiPrecision     int
+    CDataTypeCode   uint16
+}
+
+var MapDataType = map[string]uint16{
+    "INT" : 0x000E,
+}
+
+func queryAnything(db *sql.DB, query string) (string) {
+    var output string
+    keyspace := "dummykeyspace"
+    tablename := "dummytablename"
+    rows, err := db.Query(query)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer rows.Close()
+    columnNames, err := rows.Columns()
+    if err != nil {
+        panic(err)
+    }
+
+    header := fmt.Sprintf("%08x%08x%08x%04x%x%04x%x", HEADER_CODE, OPTION, len(columnNames), len(keyspace), keyspace, len(tablename), tablename)
+    output = header
+    fmt.Printf("header: <%s>\n", header)
+
+
+    columnTypes := make ([]ColumnType, len(columnNames))
+    //fmt.Printf("The column names %#v\n", columnNames)
+    for _idx, _columnName := range columnNames {
+        columnTypes[_idx].ColumnName = _columnName
+    }
+    fieldType, err := rows.ColumnTypes()
+    if err != nil {
+        panic(err)
+    }
+    for _idx, schema  := range fieldType {
+        fmt.Printf("The schema is <%s>\n", schema.DatabaseTypeName() )
+        columnTypes[_idx].TiDataType     = schema.DatabaseTypeName()
+        columnTypes[_idx].CDataTypeCode  = MapDataType[schema.DatabaseTypeName()]
+    }
+    fmt.Printf("The data is <%#v>\n", columnTypes)
+
+    for _, columnMetaData := range columnTypes {
+        columnMeta := fmt.Sprintf("%04x%x%04x", len(columnMetaData.ColumnName), columnMetaData.ColumnName, columnMetaData.CDataTypeCode) 
+        fmt.Printf("column meta data: <%s>\n", columnMeta)
+        output += columnMeta
+    }
+
+    result,err := scanner.ScanMap(rows)
+    fmt.Printf("The result is <%v> \n", result)
+    for _,record := range result {
+        fmt.Printf("The data is <%v> \n", record)
+    }
+
+    return output
+}
+
+func main() {
+    db, err := sql.Open("mysql", "cqluser:cqluser@tcp(192.168.1.108:4000)/test")
+    if err != nil {
+        panic (err) 
+        return
+    }
+    defer db.Close()
+    output := queryAnything(db, "SELECT col01, col02 FROM test01" )
+    fmt.Printf("The data is <%s>\n", output)
+}
